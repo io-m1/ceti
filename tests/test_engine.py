66717eaa9574cf66f0c21d7999cbcd5b811866f7
io-m1 <eat.instadools@gmail.com>
2026-01-21 11:43:02 +0000

    Add testing infrastructure + fix imports + refusal test

---
+++ tests/test_engine.py
@@ -0,0 +1,8 @@
+import pytest
+
+from src.engine.verification import verify_query
+
+@pytest.mark.asyncio
+async def test_verify_query_refusal():
+    response = await verify_query("Test query", "LOW")
+    assert response.authorization == "DENIED"
